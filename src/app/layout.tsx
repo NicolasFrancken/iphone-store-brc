da73fc7e19a73f1381893791e3aee7a2981a0f5b
NicolasFrancken <franckennicolas@gmail.com>
2023-10-24 10:25:12 -0300

    es

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="es">
       <Head>
         <title>IphoneStoreBRC | Reparación de Equipos Apple en Bariloche</title>
         <meta
