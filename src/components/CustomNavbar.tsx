e2cb6e1f9284916c97496ca427e710e8a887fc8a
NicolasFrancken <franckennicolas@gmail.com>
2024-01-15 15:58:24 -0300

    Commented out unused code in CustomNavbar component

--- src/components/CustomNavbar.tsx
+++ src/components/CustomNavbar.tsx
@@ -64,7 +64,7 @@ export default function CustomNavbar() {
       </NavbarItem> */}
       <NavbarMenuToggle className="md:hidden" />
       <NavbarMenu className="flex flex-col items-center justify-center">
-        <NavbarMenuItem>
+        {/* <NavbarMenuItem>
           <Button
             size="lg"
             className="font-bold text-lg bg-[#14CBEA] mb-[10px]"
@@ -73,7 +73,7 @@ export default function CustomNavbar() {
           >
             COMPRÁ
           </Button>
-        </NavbarMenuItem>
+        </NavbarMenuItem> */}
         <NavbarMenuItem>
           <Link
             isBlock
